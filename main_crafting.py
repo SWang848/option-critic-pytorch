c70c24a92c44ada6c057c7742132a79944d6a6c4
SWang848 <swang848@outlook.com>
2022-10-27 10:54:46 -0600

    add sweep

--- main_crafting.py
+++ main_crafting.py
@@ -27,7 +27,7 @@ from callbacks import WandbCallback
 from plots import save_requirement_graph, save_option_graph
 
 parser = argparse.ArgumentParser(description="Option Critic PyTorch")
-# parser.add_argument('--env', default='CartPole-v0', help='ROM to run')
+parser.add_argument('--agent', default='OptionCritc', help='agent name')
 parser.add_argument('--optimal-eps', type=float, default=0.05, help='Epsilon when playing optimally')
 parser.add_argument('--frame-skip', default=4, type=int, help='Every how many frames to process')
 parser.add_argument('--learning-rate',type=float, default=.0005, help='Learning rate')
@@ -44,7 +44,7 @@ parser.add_argument('--entropy-reg', type=float, default=0.01, help=('Regulariza
 parser.add_argument('--num-options', type=int, default=2, help=('Number of options to create.'))
 parser.add_argument('--temp', type=float, default=1, help='Action distribution softmax tempurature param.')
 
-parser.add_argument('--reward-shapping', type=int, default=RewardShaping.ALL_USEFUL, help=('shapping rewards.'))
+parser.add_argument('--reward-shapping', type=int, default=RewardShaping.DIRECT_USEFUL, help=('shapping rewards.'))
 
 parser.add_argument('--max_steps_ep', type=int, default=200, help='number of maximum steps per episode.')
 parser.add_argument('--max_steps_total', type=int, default=int(1e6), help='number of maximum steps to take.') # bout 4 million
@@ -82,41 +82,42 @@ def run(args):
     #     'switch-goal': args.switch_goal
     # }
 
-    run = wandb.init(project="OptionCritic", config={"agent":"OptionCrtic"}, monitor_gym=True)
+    run = wandb.init(project="OptionCritic", config={}, monitor_gym=True)
     timestamp = time.strftime("%Y%m%d_%H%M%S")
     run_dirname = f"{timestamp}-{run.id}"
     wandb.config.update(args)
+    config = wandb.config
 
-    env = MineCraftingEnv(max_step=args.max_steps_total, seed=args.seed)
-    task = TaskObtainItem(env.world,env.world.item_from_name["wood_plank"],reward_shaping=RewardShaping(args.reward_shapping))
+    env = MineCraftingEnv(max_step=config.max_steps_total, seed=config.seed)
+    task = TaskObtainItem(env.world,env.world.item_from_name["wood_plank"],reward_shaping=RewardShaping(eval(config.reward_shapping)))
     env.add_task(task)
 
     is_atari = False
     option_critic = OptionCriticConv if is_atari else OptionCriticFeatures
-    device = torch.device('cuda' if torch.cuda.is_available() and args.cuda else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() and config.cuda else 'cpu')
 
     option_critic = option_critic(
         in_features=env.observation_space.shape[0],
         num_actions=env.action_space.n,
-        num_options=args.num_options,
-        temperature=args.temp,
-        eps_start=args.epsilon_start,
-        eps_min=args.epsilon_min,
-        eps_decay=args.epsilon_decay,
-        eps_test=args.optimal_eps,
+        num_options=config.num_options,
+        temperature=config.temp,
+        eps_start=config.epsilon_start,
+        eps_min=config.epsilon_min,
+        eps_decay=config.epsilon_decay,
+        eps_test=config.optimal_eps,
         device=device
     )
     # Create a prime network for more stable Q values
     option_critic_prime = deepcopy(option_critic)
 
-    optim = torch.optim.RMSprop(option_critic.parameters(), lr=args.learning_rate)
+    optim = torch.optim.RMSprop(option_critic.parameters(), lr=config.learning_rate)
 
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
+    np.random.seed(config.seed)
+    torch.manual_seed(config.seed)
 
-    buffer = ReplayBuffer(capacity=args.max_history, seed=args.seed)
-    # logger = Logger(logdir=args.logdir, run_name=f"{OptionCriticFeatures.__name__}-{args.env}-{args.exp}-{time.ctime()}")
-    logger = Logger(logdir=args.logdir, run_name=f"RandomCrafting-{args.exp}-{time.ctime()}")
+    buffer = ReplayBuffer(capacity=config.max_history, seed=config.seed)
+    # logger = Logger(logdir=config.logdir, run_name=f"{OptionCriticFeatures.__name__}-{config.env}-{config.exp}-{time.ctime()}")
+    logger = Logger(logdir=config.logdir, run_name=f"RandomCrafting-{config.exp}-{time.ctime()}")
 
     # Get & save requirements graph
     requirement_graph_path = save_requirement_graph(
@@ -146,10 +147,10 @@ def run(args):
         }
     )
     steps = 0
-    if args.switch_goal: print(f"Current goal {env.goal}")
-    while steps < args.max_steps_total:
+    if config.switch_goal: print(f"Current goal {env.goal}")
+    while steps < config.max_steps_total:
 
-        rewards = 0 ; option_lengths = {opt:[] for opt in range(args.num_options)}
+        rewards = 0 ; option_lengths = {opt:[] for opt in range(config.num_options)}
 
         obs = env.reset()[0]
         
@@ -160,26 +161,26 @@ def run(args):
         # Goal switching experiment: run for 1k episodes in fourrooms, switch goals and run for another
         # 2k episodes. In option-critic, if the options have some meaning, only the policy-over-options
         # should be finedtuned (this is what we would hope).
-        if args.switch_goal and logger.n_eps == 1000:
+        if config.switch_goal and logger.n_eps == 1000:
             torch.save({'model_params': option_critic.state_dict(),
                         'goal_state': env.goal},
-                        f'models/option_critic_seed={args.seed}_1k')
+                        f'models/option_critic_seed={config.seed}_1k')
             env.switch_goal()
             print(f"New goal {env.goal}")
 
-        if args.switch_goal and logger.n_eps > 2000:
+        if config.switch_goal and logger.n_eps > 2000:
             torch.save({'model_params': option_critic.state_dict(),
                         'goal_state': env.goal},
-                        f'models/option_critic_seed={args.seed}_2k')
+                        f'models/option_critic_seed={config.seed}_2k')
             break
 
         done = False ; ep_steps = 0 ; option_termination = True ; curr_op_len = 0
-        while not done and ep_steps < args.max_steps_ep:
+        while not done and ep_steps < config.max_steps_ep:
             epsilon = option_critic.epsilon
 
             if option_termination:
                 option_lengths[current_option].append(curr_op_len)
-                current_option = np.random.choice(args.num_options) if np.random.rand() < epsilon else greedy_option
+                current_option = np.random.choice(config.num_options) if np.random.rand() < epsilon else greedy_option
                 curr_op_len = 0
     
             action, logp, entropy = option_critic.get_action(state, current_option)
@@ -189,21 +190,21 @@ def run(args):
             rewards += reward
 
             actor_loss, critic_loss = None, None
-            if len(buffer) > args.batch_size:
+            if len(buffer) > config.batch_size:
                 actor_loss = actor_loss_fn(obs, current_option, logp, entropy, \
-                    reward, done, next_obs, option_critic, option_critic_prime, args)
+                    reward, done, next_obs, option_critic, option_critic_prime, config)
                 loss = actor_loss
 
-                if steps % args.update_frequency == 0:
-                    data_batch = buffer.sample(args.batch_size)
-                    critic_loss = critic_loss_fn(option_critic, option_critic_prime, data_batch, args)
+                if steps % config.update_frequency == 0:
+                    data_batch = buffer.sample(config.batch_size)
+                    critic_loss = critic_loss_fn(option_critic, option_critic_prime, data_batch, config)
                     loss += critic_loss
 
                 optim.zero_grad()
                 loss.backward()
                 optim.step()
 
-                if steps % args.freeze_interval == 0:
+                if steps % config.freeze_interval == 0:
                     option_critic_prime.load_state_dict(option_critic.state_dict())
 
             state = option_critic.get_state(to_tensor(next_obs))
@@ -224,4 +225,23 @@ def run(args):
 
 if __name__=="__main__":
     args = parser.parse_args()
-    run(args)
+    # run(args)
+    sweep_configuration = {
+        'method': 'random',
+        'name': 'sweep',
+        'metric': {
+            'goal': 'maximize',
+            'name': 'rewards'
+        },
+        'parameters':{
+            'batch_size': {'values': [16, 32, 64]},
+            'learning_rate': {'max': 0.001, 'min': 0.0005},
+            'num_options': {'max': 10, 'min': 2},
+            'entropy_reg': {'max': 0.1, 'min': 0.01},
+            'termination_reg': {'max': 0.1, 'min': 0.01}
+        }
+
+    }
+
+    sweep_id = wandb.sweep(sweep=sweep_configuration, project='OptionCritics')
+    wandb.agent(sweep_id, function=run(args), count=10)
